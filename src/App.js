b8e3a07047ebd3611525dd0e2b31b75f9e319f31
maladr0it <bradford.lachlan@gmail.com>
2018-04-27 22:00:56 +0900

    make a grid

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
   render() {
+    const spawnSquares = () => {
+      let elementArray = [];
+      for (let i = 0; i < 10 * 20; i++) {
+        elementArray.push(
+          <div className={`Square ${i < 10 ? "Filled" : ""}`}>!</div>
+        );
+      }
+      return elementArray;
+    };
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="Board">{spawnSquares()}</div>
       </div>
     );
   }
